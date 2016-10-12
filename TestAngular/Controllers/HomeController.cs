cf844c8cd575c7c25e725f0135d35f107a60b42e
sontran <son.tran@newoceaninfosys.com>
2016-10-12 17:11:25 +0700

    -Commit edit homeController

--- TestAngular/Controllers/HomeController.cs
+++ TestAngular/Controllers/HomeController.cs
@@ -10,6 +10,10 @@ namespace TestAngular.Controllers
     {
         public ActionResult Index()
         {
+            //line 1
+            //line 2
+            //line 3
+            //line 4
             return View();
         }
         public ActionResult TestRouting()
