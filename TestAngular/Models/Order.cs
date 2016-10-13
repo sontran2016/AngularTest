5be4eb1b7302399f3f6db7dc06dce7ca1919f5e2
sontran <son.tran@newoceaninfosys.com>
2016-10-13 11:51:45 +0700

    add file order

---
+++ TestAngular/Models/Order.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TestAngular.Models
+{
+    public class Order
+    {
+        public Order()
+        {
+            CreateDate = DateTime.Now;
+        }
+
+        [Key]
+        [DatabaseGenerated( DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        public DateTime OrderDate { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        public ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+    public class OrderDetail
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string GoodCode { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public int OrderId { get; set; }
+
+        public Order Order { get; set; }
+    }
+}
