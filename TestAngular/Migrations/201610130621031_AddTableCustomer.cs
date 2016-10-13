b92e7533148aa5454c931393746e42ca47315cc2
sontran <son.tran@newoceaninfosys.com>
2016-10-13 15:12:21 +0700

    add migrate AddTableCustomer

---
+++ TestAngular/Migrations/201610130621031_AddTableCustomer.cs
@@ -0,0 +1,27 @@
+namespace TestAngular.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddTableCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FullName = c.String(nullable: false),
+                        Phone = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Customers");
+        }
+    }
+}
